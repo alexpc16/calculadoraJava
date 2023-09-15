8355f6ddfbf9b2fadb425e4034dda95a04c285e0
alexpc16 <alex16@ciencias.unam.mx>
2023-09-15 10:43:53 -0600

    agregando elementos al panel principal

--- MiVentana.java
+++ MiVentana.java
@@ -1,15 +1,33 @@
-import javax.swing.*;
+import java.awt.*;
 
+
+import javax.swing.*;
+import java.awt.Container;
 
 public class MiVentana extends JFrame {
 
     public MiVentana() {
 
         super("Titulo de ventana");
-        setSize (400, 300);
-        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Obtenemos el panel principale (contentPane) del frame para agregar elementos sobre el
+
+        Container contentPane = getContentPane();
+
+        // Definimos el modelo de layout para posicionar los elementos en el panel principal
+        contentPane.setLayout (new FlowLayout ());
+
+        JLabel etiqueta = new JLabel ("Nombre: "); JTextField texto = new JTextField (20);
+        JButton boton = new JButton ("Saludar");
+
+        //Agregamos los eleentos al panel principal
+
+        contentPane.add(etiqueta);
+        contentPane.add(boton);
+        contentPane.add(texto);
         
 
     }
-        
+
 }
