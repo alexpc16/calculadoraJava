d64f8fda2d7910fb7e08edd725513c848c1d956f
alexpc16 <alex16@ciencias.unam.mx>
2023-09-15 10:12:58 -0600

    Crear ventana

---
+++ MainCalculadora.java
@@ -0,0 +1,11 @@
+public class MainCalculadora {
+    public static void main(String[] args) {
+
+        // Crear una ventana
+
+        MiVentana ventana = new MiVentana();
+        ventana.setVisible(true);
+
+    }
+
+}
---
+++ MiVentana.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+
+public class MiVentana extends JFrame {
+
+    public MiVentana() {
+
+        super("Titulo de ventana");
+        setSize (400, 300);
+        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        
+
+    }
+        
+}
