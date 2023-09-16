cc7c341322af8d9ac31c024fe49020a51eba3ffb
alexpc16 <alex16@ciencias.unam.mx>
2023-09-15 18:58:19 -0600

    codigo para la memoria resultado en string

--- MiVentana.java
+++ MiVentana.java
@@ -1,8 +1,55 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+class EventoBotonPulsado implements ActionListener {
+    private JTextField textField;
+    private MiVentana ventana; // Agregamos una referencia a la ventana
+
+    public EventoBotonPulsado(JTextField textField, MiVentana ventana) {
+        this.textField = textField;
+        this.ventana = ventana; // Inicializamos la referencia a la ventana
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton) e.getSource();
+        String buttonText = button.getText();
+
+        System.out.println(buttonText.equals(" 1 "));
+        
+        if(buttonText.equals(" = ")){
+            ventana.resultado = textField.getText();
+
+        }
+        else if(buttonText.equals(" 1 ")){
+            textField.setText("ya" + ventana.resultado + "ya");
+            System.out.println(ventana.resultado);
+
+        }
+        else if(buttonText.equals(" 2 ")){
+            textField.setText("");
+
+        }
+        else{
+
+            textField.setText(textField.getText() + buttonText);
+        }
+
+        // Actualizamos el atributo 'resultado' de la ventana
+    }
+}
+
 public class MiVentana extends JFrame {
+
+    Container contentPane;
+
+    String resultado;
+
+    JTextField textField;
 
     public MiVentana() {
         super("Titulo de ventana");
@@ -10,40 +57,57 @@ public class MiVentana extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Obtenemos el panel principal (contentPane) del frame para agregar elementos sobre el
-        Container contentPane = getContentPane();
+        this.contentPane = getContentPane();
 
         // Definimos el modelo de layout para posicionar los elementos en el panel principal
         contentPane.setLayout(new GridBagLayout());
 
         // Definimos las restricciones para el posicionamiento de los elementos
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.insets = new Insets(1, 1, 10, 1);
 
-        // Creamos un cuadro de texto con un ancho de 10 caracteres
-        JTextField textField = new JTextField();
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.gridwidth = 3; // Se expande para ocupar 4 columnas
-        constraints.fill = GridBagConstraints.HORIZONTAL; // Expandir horizontalmente
-        
-        textField.setBorder(new EmptyBorder(10,20,30,40));
-        
-        contentPane.add(textField, constraints);
+        crearCuadroTexto();
+        crearBotones();
+        crearBotonIgual();
 
+        // No es necesario inicializar 'resultado' aquí
 
-        // Creamos un botón personalizado usando la clase BotonNumero
+    }
+
+    private void crearBotonIgual() {
+        Boton botonIgual = new Boton(" = ", 100, 50, 2, 2);
+        contentPane.add(botonIgual, botonIgual.constraintsB);
+
+        botonIgual.addActionListener(new EventoBotonPulsado(textField, this)); // Pasamos la referencia a la ventana
+    }
+
+    private void crearBotones() {
+        // Creamos un botón personalizado usando la clase Boton
         Boton boton1 = new Boton(" 1 ", 100, 50, 0, 1);
         Boton boton2 = new Boton(" 2 ", 100, 50, 1, 1);
-        Boton boton3 = new Boton(" 3 ", 100, 50, 2, 2);
-
 
         // Agregamos el botón personalizado al panel principal
         contentPane.add(boton1, boton1.constraintsB);
         contentPane.add(boton2, boton2.constraintsB);
-        contentPane.add(boton3, boton3.constraintsB);
 
+        boton1.addActionListener(new EventoBotonPulsado(textField, this)); // Pasamos la referencia a la ventana
+        boton2.addActionListener(new EventoBotonPulsado(textField, this)); // Pasamos la referencia a la ventana
 
     }
 
+    private void crearCuadroTexto() {
 
+        // Creamos un cuadro de texto con un ancho de 10 caracteres
+        textField = new JTextField();
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(1, 1, 10, 1);
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 3; // Se expande para ocupar 4 columnas
+        constraints.fill = GridBagConstraints.HORIZONTAL; // Expandir horizontalmente
+
+        textField.setBorder(new EmptyBorder(10, 20, 30, 40));
+
+        contentPane.add(textField, constraints);
+    }
 }
---
+++ Resolver.java
@@ -0,0 +1,115 @@
+import java.util.Stack;
+
+public class Resolver {
+        public static double evaluarExpresion(String expresion) {
+        // Eliminar espacios en blanco de la expresión
+        expresion = expresion.replaceAll("\\s+", "");
+
+        // Crear una pila para operadores y otra para operandos
+        Stack<Double> operandos = new Stack<>();
+        Stack<Character> operadores = new Stack<>();
+
+        // Definir la precedencia de los operadores
+        // Puedes ajustar estas prioridades según tus necesidades
+        final int PRIORIDAD_SUMA_RESTA = 1;
+        final int PRIORIDAD_MULTIPLICACION_DIVISION = 2;
+
+        // Dividir la expresión en tokens (números, operadores y paréntesis)
+        String[] tokens = expresion.split("(?<=[-+*/()])|(?=[-+*/()])");
+
+        for (String token : tokens) {
+            if (token.isEmpty()) {
+                // Ignorar tokens vacíos
+                continue;
+            } else if (esNumero(token)) {
+                // Si el token es un número, apílalo en operandos
+                operandos.push(Double.parseDouble(token));
+            } else if (token.charAt(0) == '(') {
+                // Si el token es un paréntesis izquierdo, apílalo en operadores
+                operadores.push('(');
+            } else if (token.charAt(0) == ')') {
+                // Si el token es un paréntesis derecho, desapila operadores hasta encontrar '('
+                while (!operadores.isEmpty() && operadores.peek() != '(') {
+                    evaluarOperador(operandos, operadores);
+                }
+                // Retira el '(' de la pila de operadores
+                if (!operadores.isEmpty() && operadores.peek() == '(') {
+                    operadores.pop();
+                }
+            } else {
+                // El token es un operador (+, -, *, /)
+                while (!operadores.isEmpty() && obtenerPrioridad(operadores.peek()) >= obtenerPrioridad(token.charAt(0))) {
+                    evaluarOperador(operandos, operadores);
+                }
+                operadores.push(token.charAt(0));
+            }
+        }
+
+        // Procesar los operadores restantes en la pila de operadores
+        while (!operadores.isEmpty()) {
+            evaluarOperador(operandos, operadores);
+        }
+
+        // El resultado final debe estar en la cima de la pila de operandos
+        if (!operandos.isEmpty()) {
+            return operandos.peek();
+        } else {
+            // La expresión estaba vacía o mal formada
+            throw new IllegalArgumentException("Expresión no válida");
+        }
+    }
+
+    // Función auxiliar para determinar si un token es un número
+    private static boolean esNumero(String token) {
+        try {
+            Double.parseDouble(token);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    // Función auxiliar para obtener la prioridad de un operador
+    private static int obtenerPrioridad(char operador) {
+        switch (operador) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return 0; // Para otros caracteres o paréntesis
+        }
+    }
+
+    // Función auxiliar para evaluar un operador y realizar la operación correspondiente
+    private static void evaluarOperador(Stack<Double> operandos, Stack<Character> operadores) {
+        if (operandos.size() < 2 || operadores.isEmpty()) {
+            throw new IllegalArgumentException("Expresión no válida");
+        }
+        double b = operandos.pop();
+        double a = operandos.pop();
+        char operador = operadores.pop();
+        switch (operador) {
+            case '+':
+                operandos.push(a + b);
+                break;
+            case '-':
+                operandos.push(a - b);
+                break;
+            case '*':
+                operandos.push(a * b);
+                break;
+            case '/':
+                if (b == 0) {
+                    throw new ArithmeticException("División por cero");
+                }
+                operandos.push(a / b);
+                break;
+            default:
+                throw new IllegalArgumentException("Operador no válido");
+        }
+    }
+
+}
